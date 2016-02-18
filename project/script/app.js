e3c35e6ad8178194f3d20b8df6ae9e71cdcbb02a
martin-varbanov96 <martin_varbanov96@abv.bg>
2016-02-18 16:00:41 +0200

    0.5

--- project/script/app.js
+++ project/script/app.js
@@ -14,12 +14,71 @@ $(document).ready(function(){
     //might have to change click() -> live()
     $("#main-menu").click(function(){
         $(".dead-square, .alive-square").each(function(){
-            setInMatrix(this);
-
+            setInMatrix(this);            
         });
-            console.log(arrayElements);
+        for(var i = 0; i < Math.sqrt(arraySize); i++){
+            for(var j = 0; j < Math.sqrt(arraySize); j++){
+                if(arrayElements[i][j] == 0){
+                    deadTransition(i, j);
+                }else if(arrayElements[i][j] == 1){
+                    aliveTransition(i, j);
+                }                
+            }            
+        }
+        console.log(arrayElements);
 
     });
+
+    // checks if any alive transition can be made
+    function aliveTransition(i, j){
+        var count = 0;
+        for(var row = i-1; row <= i+1; row++){
+            for(var col = j-1; col <= j + 1; col++){
+                if(
+                    ((row == i ) && (col == j)) ||
+                    (row < 0 ) || 
+                    (row >= Math.sqrt(arraySize) ||
+                    (col < 0 ) || 
+                    (col >= Math.sqrt(arraySize))
+                   ) 
+                  ){
+                    continue;
+                }
+                if(arrayElements[row][col] == 1 ){
+                    count++;
+                }
+            }
+        }
+        console.log(count);
+        if((count != 3) && (count != 2)){
+            $("#" + i + "" + j).attr("class", "dead-square");
+        }
+        
+    }
+    //checks if any deadTransition can be made
+    function deadTransition(i, j){
+        var count = 0;
+        for(var row = i-1; row <= i+1; row++){
+            for(var col = j-1; col <= j + 1; col++){
+                if(
+                    ((row == i ) && (col == j)) ||
+                    (row < 0 ) || 
+                    (row >= Math.sqrt(arraySize) ||
+                    (col < 0 ) || 
+                    (col >= Math.sqrt(arraySize))
+                   ) 
+                  ){
+                    continue;
+                }
+                if(arrayElements[row][col] == 1 ){
+                    count++;
+                }
+            }
+        }
+        if(count == 3){
+            $("#" + i + "" + j).attr("class", "alive-square");
+        }
+    }
     
     //sets the value of the square in the main matrix
     function setInMatrix(elementId){
